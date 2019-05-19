d459feaf978f89f94b2cfdf8880b95c56a4e31b9
Breno Barnard <brenobarnard1997@gmail.com>
2019-05-19 15:17:49 -0300

    Retoques do Front

--- application/views/pages/cars.php
+++ application/views/pages/cars.php
@@ -1,19 +1,24 @@
 <div class="title">
   <span><?php echo $title ?></span>
 </div>
-
+<div class="tip">
+  <span><b><i class="icon ion-md-help-circle-outline"></i></b> Put the mouse cursor over the vehicle to see details.</span>
+</div>
 <div class="cars-container">
 
   <?php foreach ($cars as $car_item): ?>
     <div class="car-item">
+      <div class="car-description">
+        <span><b>Model: </b><span class="car-model"><?php echo $car_item['model'] ?></span></span>
+        <span><b>Year: </b><?php echo $car_item['year'] ?></span>
+        <span><b>Color: </b><?php echo $car_item['color'] ?></span>
+        <span><b>Category: </b><?php echo $car_item['category'] ?></span>
+      </div>
       <img src="<?php echo base_url('upload/'.$car_item['img'].'.jpg') ?>" alt="Foto de <?php echo $car_item['model']?>">
-      <span><b>Model: </b><span class="car-model"><?php echo $car_item['model'] ?></span></span>
-      <span><b>Year: </b><?php echo $car_item['year'] ?></span>
-      <span><b>Color: </b><?php echo $car_item['color'] ?></span>
-      <span><b>Category: </b><?php echo $car_item['category'] ?></span>
     </div>
   <?php endforeach; ?>
   
+  <div style="width: fill-availabe"></div>
 </div>
 
 
--- application/views/templates/main_header.php
+++ application/views/templates/main_header.php
@@ -9,6 +9,7 @@
 
 	<link rel="stylesheet" href="<?php echo base_url('assets/styles/main.css') ?>" type="text/css">
 	<link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400,600,700,900&display=swap" rel="stylesheet">
+	<link href="https://unpkg.com/ionicons@4.5.5/dist/css/ionicons.min.css" rel="stylesheet">
 </head>
 
 <body>
