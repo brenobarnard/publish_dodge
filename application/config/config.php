178faa6524598eba457890b4383a960830a8086e
Breno Barnard <42187458+brenobarnard@users.noreply.github.com>
2019-05-22 20:19:39 -0300

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://php-dodgeshowroom.apps.us-east-2.online-starter.openshift.com/';
+$config['base_url'] = 'http://dodgeshow-dodgeshow.apps.us-east-2.online-starter.openshift.com';
 
 /*
 |--------------------------------------------------------------------------
