df9ce836022d067caa60f419a6ba1ef3ec5941d0
Breno Barnard <42187458+brenobarnard@users.noreply.github.com>
2019-05-22 20:35:41 -0300

    Update main_header.php

--- application/views/templates/main_header.php
+++ application/views/templates/main_header.php
@@ -11,6 +11,7 @@
 		<link rel="stylesheet" href="<?php echo base_url('assets/styles/'.$style) ?>" type="text/css">
 	<?php endforeach;?>
 	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400,600,700,900&display=swap" rel="stylesheet">
 	<link href="https://unpkg.com/ionicons@4.5.5/dist/css/ionicons.min.css" rel="stylesheet">
 </head>
